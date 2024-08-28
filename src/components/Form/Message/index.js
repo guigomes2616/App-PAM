2f439e9897a4cf50b276987c101a7dd3a2d6396c
Guilherme <guilherme.silva2616@etec.sp.gov.br>
2024-08-27 22:25:03 -0300

    att27.08

--- src/components/Form/Message/index.js
+++ src/components/Form/Message/index.js
@@ -1,23 +1,35 @@
 import React, {useState, useEffect} from 'react';
-import { TextInput, Text, Pressable, View, FlatList} from 'react-native'
+import { TextInput, Text, Pressable, View, FlatList} from 'react-native';
+import socket from "../../../socket"
+import style from './style';
 
 export default function Message() {
-    const [message, setMessage] = useState('')
-    const [messageList, setMessageList] = useState('')
+    const [message, sendMessage] = useState("")
+    const [messageList, setMessageList] = useState([])
 
     function sendMessage() {
+        console.log("entrou na sendMessage")
 
+        setMessageList{(arr) => [...arr, message]};
+
+        setMessageList("")
     }
 
     return(
         <View>
-            <Text>Digite uma mensagem:</Text>
-            <TextInput/>
-
+            <Text style={style.title}>Digite uma mensagem:</Text>
+            <TextInput
+                onChangeText={message}
+                placeholder="Escreva sua mensagem"
+                inputMode="text"
+                value={message}
+            />
+                
             <Pressable
                 title="Enviar"
                 onPress={() => sendMessage()}
             />
+            <Text>Enviar</Text>
 
             <Text> Mensagem enviadas:</Text>
             <FlatList
---
+++ src/components/Form/Message/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native-web";
+
+const style = StyleSheet.create({
+    button:{
+        backgroundColor:"#a0a0a0", 
+        margin:30,
+        paddingBottom:14,
+        paddingTop:14,
+        borderRadius: 35,
+        alignItems: "center",
+    },
+    title:{
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    inputMessage:{
+        borderRadius: 25,
+        backgroundColor: "#f6f6f6",
+    
+    }
+})
+
+export default style
